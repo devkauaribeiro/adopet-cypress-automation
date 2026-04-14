2d34875f3da8fdc89b7c6e4f83c87d2e655e79b2
devkauaribeiro <kauaribeirodev@outlook.com>
2026-04-14 19:22:09 -0300

    seguranca de dados

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,11 +1,10 @@
 describe('Api Adopet', () => {
-  const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTc3NjE5NjQ5NiwiZXhwIjoxNzc2NDU1Njk2fQ.8j1w8BOj_ZJn56_UgOA8jmHz_JVhaPolIoIOMoKzsv8`
 
   it('Mensagens da API', () => {
     cy.request({
       method: 'GET',
       url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
-      headers: { authorization }
+      headers: Cypress.env()
     }).then((res) => {
       expect(res.status).to.be.equal(200)
       expect(res.body).is.not.empty
